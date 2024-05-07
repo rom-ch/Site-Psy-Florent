5221fff0ee97eb1d9b273b40157a84411c2ad8b7
Romain Chauvin <rom.chauvin@gmail.com>
2024-05-07 10:51:25 +0200

    img cab

--- src/pages/Office/Office.jsx
+++ src/pages/Office/Office.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet";
 import styles from "./Office.module.css";
-import cabImg from "../../assets/cabinet-img.webp";
+import cabImg from "../../assets/cab-img.webp";
+import buildingImg from '../../assets/building-img.webp';
 
 function Office() {
   return (
@@ -40,15 +41,28 @@ function Office() {
                 <strong>dans le quartier des ponceaux</strong> à Cergy. Je
                 partage actuellement le cabinet avec une psychiatre.
               </p>
-              <span>
+              {/* <span>
                 <strong>Adresse : </strong>8 rue du ponceau, 95000 Cergy (2ème
                 étage)
-              </span>
+              </span> */}
             </div>
             <div className={styles["cab-img"]}>
               <img src={cabImg} alt="Photo du cabinet" />
             </div>
           </div>
+
+          <div className={styles.cab}>
+            <div className={styles["cab-img"]}>
+              <img src={buildingImg} alt="Photo du cabinet" />
+            </div>
+            <div className={styles["cab-content"]}>
+              <p>
+                <strong>Adresse : </strong>8 rue du ponceau, 95000 Cergy (2ème
+                étage)
+              </p>
+            </div>
+          </div>
+
           <div className={styles.map}>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1307.7681575450606!2d2.0677677444907165!3d49.03842539550331!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6f520b7df7595%3A0x64af1d71f22b01e7!2s8%20Rue%20du%20Ponceau%2C%2095000%20Cergy!5e0!3m2!1sen!2sfr!4v1714989546486!5m2!1sen!2sfr"
